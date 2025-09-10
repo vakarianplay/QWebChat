1f7af89f99cadb7bc46eb8c5772112d7e55f117e
Kostya Vakarian <vakarianplay@gmail.com>
2025-09-10 11:56:27 +0300

    Arguments for setup wsPort and httpPort

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <QCoreApplication>
+#include <QString>
 #include "chatserver.h"
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
-    // Создаем объект сервера
-    quint16 wsPort = 9098;    // Порт WebSocket
-    quint16 httpPort = 9099;  // Порт HTTP
+    quint16 httpPort = 8080;
+    quint16 wsPort = 8081;
+
+    QStringList arguments = app.arguments();
+    if (arguments.size() > 1) {
+        for (int i = 1; i < arguments.size(); ++i) {
+            QString arg = arguments[i];
+            if (arg.startsWith("--http=")) {
+                httpPort = arg.mid(QString("--http=").size()).toUShort();
+            } else if (arg.startsWith("--ws=")) {
+                wsPort = arg.mid(QString("--ws=").size()).toUShort();
+            }
+        }
+    }
+
+    qDebug() << "Run ChatServer:" << "\nHTTP port:" << httpPort << "\nWebSocket port:" << wsPort;
+
     ChatServer chatServer(wsPort, httpPort);
 
     return app.exec();
