8f0d8d1be0f6498395938d90fd33bb964766bd4f
Kostya Vakarian <vakarianplay@gmail.com>
2025-11-24 10:39:55 +0300

    Update pro-file

--- ChatServerQT.pro
+++ ChatServerQT.pro
@@ -7,8 +7,11 @@ CONFIG += c++17 cmdline
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+        messageprocessor.cpp \
+        filemanager.cpp \
         chatserver.cpp \
         main.cpp
+
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
@@ -16,7 +19,9 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
-    chatserver.h
+        messageprocessor.h \
+        filemanager.h \
+        chatserver.h
 
 DISTFILES += \
     index.html
--- main.cpp
+++ main.cpp
@@ -2,27 +2,24 @@
 #include <QString>
 #include "chatserver.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QCoreApplication app(argc, argv);
 
     quint16 httpPort = 8080;
     quint16 wsPort = 8081;
 
     QStringList arguments = app.arguments();
-    if (arguments.size() > 1) {
-        for (int i = 1; i < arguments.size(); ++i) {
-            QString arg = arguments[i];
-            if (arg.startsWith("--http=")) {
-                httpPort = arg.mid(QString("--http=").size()).toUShort();
-            } else if (arg.startsWith("--ws=")) {
-                wsPort = arg.mid(QString("--ws=").size()).toUShort();
-            }
+    for (int i = 1; i < arguments.size(); ++i) {
+        if (arguments[i].startsWith("--http=")) {
+            httpPort = arguments[i].mid(7).toUShort();
+        } else if (arguments[i].startsWith("--ws=")) {
+            wsPort = arguments[i].mid(5).toUShort();
         }
     }
 
-    qDebug() << "Run ChatServer:" << "\nHTTP port:" << httpPort << "\nWebSocket port:" << wsPort;
+    qDebug() << "Starting ChatServer:\nHTTP port:" << httpPort << "\nWebSocket port:" << wsPort;
 
-    ChatServer chatServer(wsPort, httpPort);
-
+    ChatServer server(wsPort, httpPort);
     return app.exec();
 }
